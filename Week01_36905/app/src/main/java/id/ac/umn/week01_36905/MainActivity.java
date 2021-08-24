f97b25daf037abb01204a05febf4eb20fb42904d
ricoferdinandd <rico.ferdinand@student.umn.ac.id>
2021-08-24 15:23:32 +0700

    week 1

---
+++ Week01_36905/app/src/main/java/id/ac/umn/week01_36905/MainActivity.java
@@ -0,0 +1,15 @@
+package id.ac.umn.week01_36905;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.RecyclerView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
