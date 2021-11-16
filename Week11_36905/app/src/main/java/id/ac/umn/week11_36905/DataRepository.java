19525032bc23e25dd4e8b0fa5ed486073d2acde0
ricoferdinandd <rico.ferdinand@student.umn.ac.id>
2021-11-17 00:16:52 +0700

    Week11

---
+++ Week11_36905/app/src/main/java/id/ac/umn/week11_36905/DataRepository.java
@@ -0,0 +1,17 @@
+package id.ac.umn.week11_36905;
+
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+public class DataRepository {
+    static String placeholderUrl = "https://jsonplaceholder.typicode.com/";
+
+    static final PostServices create() {
+        Retrofit retrofit = new Retrofit.Builder()
+                .addConverterFactory(GsonConverterFactory.create())
+                .baseUrl(placeholderUrl)
+                .build();
+
+        return retrofit.create(PostServices.class);
+    }
+}
